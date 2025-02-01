37d3622d4bbf72011f3c542508b1fe8ddd0df17a
aynabelza <169131507+aynabelza@users.noreply.github.com>
2025-02-01 15:28:42 +1100

    structure  commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html",      // This is where your HTML file is located
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
